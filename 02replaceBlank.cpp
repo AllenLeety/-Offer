43f8bf0e87e22ae5310ddab968f7ac1b741a474e
AllenLeety <allen930422@gmail.com>
2019-04-23 23:25:14 +0800

    替换空格

---
+++ 02replaceBlank.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void replaceSpace(char *str, int length)
+{
+    if(str == NULL && length<=0){
+        return;
+    }
+    int count=0;//空格数
+    int originalLen=0;//字符串实际长度
+    int i=0;
+    while(str[i] != '\0'){
+        originalLen++;
+        if(str[i] == ' '){
+            count++;
+        }
+        i++;
+    }
+
+    int newLen = originalLen + count * 2;
+    int p1 = originalLen;//原始数组最后一个元素的下标
+    int p2 = newLen;    //新数组最后一个元素的下标
+    while(p1>=0 && p2>p1){
+        if(str[p1] == ' '){
+            str[p2--] = '0';
+            str[p2--] = '2';
+            str[p2--] = '%';
+        }else{
+            str[p2--] = str[p1];
+        }
+        p1 --;
+    }
+
+}
+
+int main()
+{
+    char str[10] = "a b c d e";
+    cout << "original string:" << endl;
+    //for(int i=0; i<10; i++){
+    //    cout << str[i];
+    //}
+    cout << str << endl;
+
+    replaceSpace(str, 10);
+    cout << "replace string:" << endl;
+    cout << str << endl;
+
+    return 0;
+}
