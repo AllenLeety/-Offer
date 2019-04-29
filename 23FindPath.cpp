e734ffa7004b38024ad649432b95a7a0438d7c27
AllenLeety <allen930422@gmail.com>
2019-04-29 16:14:14 +0800

    二叉树中和为某一值的路径

---
+++ 23FindPath.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x = 0)
+    :val(x), left(NULL), right(NULL)
+    {
+    }
+};
+
+class Solution
+{
+public:
+    vector< vector<int> > m_res;
+
+    vector< vector<int> > FindPath(TreeNode* root, int expectNumber)
+    {
+        if(root == NULL)
+        {
+            return m_res;
+        }
+        vector<int> path;
+        FindPath(root, expectNumber, path, 0);
+
+        return m_res;
+    }
+
+    void FindPath(TreeNode* root, int expectNumber, vector<int> path, int currentSum)
+    {
+        currentSum += root->val;
+        path.push_back(root->val);
+        ///
+        if(currentSum == expectNumber
+        && ((root->left == NULL && root->right == NULL)))
+        {
+            for(int i = 0; i < path.size( ); i++)
+            {
+                cout <<path[i] <<" ";
+            }
+            cout <<endl;
+
+            m_res.push_back(path);
+        }
+
+        if(root->left != NULL)
+        {
+            FindPath(root->left, expectNumber, path, currentSum);
+        }
+        if(root->right != NULL)
+        {
+            FindPath(root->right, expectNumber, path, currentSum);
+        }
+    }
+
+};
+
+int main( )
+{
+    //  0  1  2  3 4
+    //  {10,5,12,4,7},22
+    TreeNode tree[5];
+    tree[0].val = 10;
+    tree[0].left = &tree[1];
+    tree[0].right = &tree[2];
+
+    tree[1].val = 5;
+    tree[1].left = &tree[3];
+    tree[1].right = &tree[4];
+
+    tree[2].val = 12;
+    tree[2].left = NULL;
+    tree[2].right = NULL;
+
+    tree[3].val = 4;
+    tree[3].left = NULL;
+    tree[3].right = NULL;
+
+    tree[4].val = 7;
+    tree[4].left = NULL;
+    tree[4].right = NULL;
+
+    Solution solu;
+    vector< vector<int> > res = solu.FindPath(&tree[0], 22);
+
+    return 0;
+}
