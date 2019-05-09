b57e0cc7cc9b1780a4eccb818b4212c1142ad5eb
AllenLeety <allen930422@gmail.com>
2019-05-09 14:59:06 +0800

    和为S的连续正数序列

---
+++ 39FindContinuousSequence.cpp
@@ -0,0 +1,17 @@
+/*
+输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int> > FindContinuousSequence(int sum)
+{
+    
+}
+
+int main()
+{
+    
+    return 0;
+}
